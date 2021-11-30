ebc4854f938880ab1ea438012aa2a49191e1def9
nfathan <athan.354@gmail.com>
2021-11-30 17:29:56 +0800

    mengangkat useNavigate

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,41 +1,56 @@
-import { useNavigate } from "react-router-dom"
+// import { useNavigate } from 'react-router-dom'
+import {
+  MdOutlineEdit,
+  MdDeleteForever,
+  MdDoneAll,
+  MdRemoveDone } from 'react-icons/md'
+import cleanCode from '../images/clean-code.jpg'
 
 function Book(props) {
-  let navigate = useNavigate()
+  // let navigate = useNavigate()
   
-  const navigateToEditBook = () => {
-    navigate("/EditBook")
-  }
+  // const navigateToEditBook = () => {
+  //   navigate("editbook")
+  // }
 
   return (
     <li className="Book shadow">
+      <div className="Book-img">
+        
+        <img src={cleanCode} alt="clean-code" />
+      </div>
       <div className="Book-header">
-        <h4>{props.bookTitle}</h4>
+        <h3>{props.bookTitle}</h3>
         {/* <button onClick={() => navigate("/EditBook")}>Edit</button> */}
-        <button onClick={() => props.editBook(props.book, navigateToEditBook)}>Edit</button>
+        
       </div>
       <div className="Book-info">
         <p>Penulis : {props.bookAuthor}</p>
         <p>Tahun : {props.bookYear}</p>
       </div>
-      <div className="Book-button center">
+      <div className="Book-button center mt-10">
         {props.isComplete ? 
           <button 
             className="complete-button" 
             onClick={() => props.toggleIsComplete(props.id, props.isComplete)}>
-              Belum Selesai
+              <MdRemoveDone /> Belum Selesai
           </button>
           : 
           <button 
             className="complete-button" 
             onClick={() => props.toggleIsComplete(props.id, props.isComplete)}>
-              Selesai
+              <MdDoneAll />Selesai
           </button> 
         }
         <button 
+          className="edit-button"
+          onClick={() => props.editBook(props.book)}>
+            <MdOutlineEdit />
+        </button>
+        <button 
           className="delete-button"
           onClick={() => props.deleteBook(props.id)}>
-            Hapus
+            <MdDeleteForever />
         </button>
       </div>
     </li>
