ef141ab44c61860de3d7edd5a1099aae8aa9bc65
nfathan <athan.354@gmail.com>
2021-12-07 22:01:18 +0800

    updated

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,34 @@
+import { useEffect, useRef } from "react"
+import { useGlobalContext } from "../context"
+
 function SearchBar() {
+  const { setSearch } = useGlobalContext()
+  const searchValue = useRef('')
+
+  useEffect(() => {
+    searchValue.current.focus()
+  }, [])
+
+  const searchBook = () => {
+    setSearch(searchValue.current.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div className="SearchBar">
-      <input 
-        type="search" 
-        id="inputSearch"
-        placeholder="Cari Buku.."
-        // onKeyUp="btnSearchClicked()"
-      />
-    </div> 
+    <form onSubmit={handleSubmit}>
+      <div className="SearchBar">
+        <input 
+          type="search" 
+          id="inputSearch"
+          placeholder="Cari Buku.."
+          ref={searchValue}
+          onChange={searchBook}
+        />
+    </div>
+    </form> 
   )
 }
 
